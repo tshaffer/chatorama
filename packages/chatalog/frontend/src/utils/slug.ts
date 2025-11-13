facd14f8bf8c2a4bb5951d0597ab959c37226606
Ted Shaffer <shaffer.family@gmail.com>
2025-11-13 05:53:07 -0800

    Delete unused files

--- packages/chatalog/frontend/src/utils/slug.ts
+++
@@ -1,6 +0,0 @@
-// frontend/src/utils/slug.ts
-export function getIdFromSlug(slug: string | undefined): string | undefined {
-  if (!slug) return undefined;
-  // we encode as "<id>-<slugified-name>"; take the leading id
-  return slug.split('-')[0];
-}
